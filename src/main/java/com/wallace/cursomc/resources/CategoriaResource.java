95ca5f5e09c5a49099e0a65280b147718b207f72
Wallace Junior <wallaceoliveirarn@gmail.com>
2018-09-01 21:56:55 -0300

    Testando o Rest

---
+++ src/main/java/com/wallace/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,28 @@
+package com.wallace.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Rest está funcionando!";
+	}
+	
+	@RestController
+	@RequestMapping(value="/teste")
+	public class teste{
+		
+		@RequestMapping(method=RequestMethod.GET)
+		public String Testando() {
+			return "Teste deu certo, Junior!";
+		}
+		
+	}
+	
+
+}
